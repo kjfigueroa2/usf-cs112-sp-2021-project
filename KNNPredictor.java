a204e2e50704f92f6e0c394c2f0a793af2568a71
kjfigueroa2 <80305067+kjfigueroa2@users.noreply.github.com>
2021-05-06 11:17:21 -0700

    Adding Project Part 3 & Improved Project Part 2
    Improvements to Part 2:
    Added comments for clarification
    Added splitToTestOrTrain boolean
    Condensed getDistance 
    Redid getPrecision & getAccuracy

--- KNNPredictor.java
+++ KNNPredictor.java
@@ -4,220 +4,219 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.lang.Math;
 import java.util.List;
 
 public class KNNPredictor extends Predictor {
+	
+	//instance variables
 	private int k;
-	public KNNPredictor(int k) {
-		this.k = k;
-	}
-	
-	
 	private static int survived = 0;
-	private static int died = 1;
-	private static int age = 0;
-	private static int fare = 0;
-	private static int dataSurvival = 0;
-	
 	private int numSurvived = 0;
 	private int numDied = 0;
+	private int recordsNotUsed = 0;
+	private int truePositive = 0;
+	private int falsePositive = 0;
+	private int falseNegative = 0;
+	private int trueNegative = 0;
+	private int lineNum = 0;
+	private boolean isTest;
+	private double pointLabel;
+	private double testLabel;
+	private double accuracy;
+	private double precision;
+	private String survivalStr;
+	private String ageStr;
+	private String fareStr;
+	private double age = 0.0;
+	private double fare = 0.0;
+	private double total;
 	
+	
+	//constructor that takes in a value for K and store K as a private int as a field
+		public KNNPredictor(int k) {
+			this.k = k;
+		}
+		
 	private ArrayList<DataPoint> dataSet = new ArrayList<DataPoint>();
 	
-	public ArrayList<DataPoint> readData(String filename) {
-		ArrayList<DataPoint> result = new ArrayList<DataPoint>();
-		
-		File file = new File(filename);
-		Scanner sc;
-		try {
-			sc = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-			return result;
+	//implementing readData
+	private List<String> lineRecord(String line){
+		List<String> values = new ArrayList<String>();
+		try(Scanner row = new Scanner(line)) {
+			row.useDelimiter(",");
+			while(row.hasNext()) {
+				values.add(row.next());
+			}
 		}
-		
-		int line = 0;
-		int recordsNotUsed = 0;
-		
-		Random random = new Random();
-		
-		sc.nextLine();
-		
-		while (sc.hasNextLine()) {
-			line++;
-			List<String> dataRecords = recordLine(sc.nextLine());
-			
-			if (dataRecords.size() <7) {
-				recordsNotUsed++;
-				continue;
-			}
-			
-			String survivalStr = dataRecords.get(dataSurvival);
-			String ageStr = dataRecords.get(age);
-			String fareStr = dataRecords.get(fare);
-			double age = 0.0;
-			double fare = 0.0;
-			int alive = 1;
-			
-			try {
-				age = Double.parseDouble(ageStr);
-			} catch (NumberFormatException ex) {
-				recordsNotUsed++;
-				continue;
-			}
-			
-			try {
-				fare =Double.parseDouble(fareStr);
-			} catch (NumberFormatException ex) {
-				recordsNotUsed++;
-				continue;
-			}
-			
-			try {
-				survived = Integer.parseInt(survivalStr);
-			} catch (NumberFormatException ex) {
-				recordsNotUsed++;
-				continue;
-			}
-			
-			boolean isTrainingData = random.nextDouble() < 0.9;
-			
-			if(isTrainingData) {
-				if(alive == survived) {
-					numSurvived++;
-				} else {
-					numDied++;
-				}
-			}
-			DataPoint datapoint = new DataPoint(age, fare, alive, isTrainingData);
-			result.add(datapoint);
-		}
-		
-		sc.close();
-		
-		System.out.println("Data Set " +"\nTotal Records: " + line +"\nRecords Not Used: " + recordsNotUsed + "\nTraining Data Total: " + (numSurvived-numDied) + 
-				"\nTest Data: " + ((line-recordsNotUsed)-(numSurvived-numDied)) + "\nTraining Data \nNumber of survivors: " + numSurvived + "\nNumber of deaths: " +numDied);
-		
-		return result;
+		return values;
 	}
 	
-		private List<String> recordLine(String lineNumber) {
-		
-		List<String> numbers = new ArrayList<String>();
-		try(Scanner rowScanner = new Scanner(lineNumber))	{
-			rowScanner.useDelimiter(",");
-			while (rowScanner.hasNext()) {
-				numbers.add(rowScanner.next());
+	@Override
+	ArrayList<DataPoint> readData(String filename){
+		File file = new File(filename);
+		ArrayList<DataPoint> dataList = new ArrayList<DataPoint>();
+		try {
+			Scanner sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				lineNum++;
+				List<String> passData = lineRecord(sc.nextLine());
+				
+				if(passData.size()<7) {
+					recordsNotUsed++;
+					continue;
+				}
+				//
+				survivalStr = passData.get(1);
+				ageStr = passData.get(5);
+				fareStr = passData.get(6);
+				
+				try {
+					age = Double.parseDouble(ageStr);
+				} catch (NumberFormatException ex) {
+					continue;
+				}
+				
+				try {
+					fare = Double.parseDouble(fareStr);
+				} catch (NumberFormatException ex) {
+					continue;
+				}
+				
+				try {
+					survived = Integer.parseInt(survivalStr);
+				} catch (NumberFormatException ex) {
+					continue;
+				}
+				
+				if(splitToTestOrTrain()) {
+					if (survived == 1 ) {
+						numSurvived++;
+					} else if (survived == 0){
+						numDied++;
+					}
+				}
+				DataPoint datapoint = new DataPoint(age, fare, survived, splitToTestOrTrain());
+				dataList.add(datapoint);
 			}
+			sc.close();
+			System.out.println("Data Set " +"\nTotal Records: " + lineNum +"\nRecords Not Used: " + recordsNotUsed + "\nTraining Data Total: " + (numSurvived-numDied) + 
+					"\nTest Data: " + ((lineNum-recordsNotUsed)-(numSurvived-numDied)) + "\nTraining Data \nNumber of survivors: " + numSurvived + "\nNumber of deaths: " +numDied);
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
 		}
-		return numbers;
-}
+		return dataList;
+	}
+	
+	public boolean splitToTestOrTrain() {
+		Random random = new Random();
+		double randomNumber = random.nextDouble();
+		
+		if (randomNumber<0.9) {
+			isTest = true;
+		} else {
+			isTest = false;
+		}
+		return isTest;
+	}
+	
+	//implement test method
 	@Override
 	public String test(DataPoint data) {
-		if (!data.isTest()) {
-			return "Data given is not a test data point.";
-		}
-		int trainingDataNum = numSurvived +numDied;
+		int trainingDataNum = numSurvived + numDied;
 		Double[][] arr = new Double[trainingDataNum][2];
-		for (int row = 0; row<trainingDataNum; row++) {
-			DataPoint trainingData=dataSet.get(row);
-			double distance = getDistance(data,trainingData);
+		for (int row = 0; row < trainingDataNum; row++) {
+			DataPoint trainingData = dataSet.get(row);
+			double distance = getDistance(data, trainingData);
 			double label = trainingData.getLabel();
 			arr[row][0] = distance;
 			arr[row][1] = label;
 		}
 		
-		java.util.Arrays.sort(arr, new java.util.Comparator<Double[]>(){
-			public int compare(Double[] a, Double[] b) {
+	java.util.Arrays.sort(arr, new java.util.Comparator<Double[]>() {
+		public int compare(Double[] a, Double[] b) {
 			return a[0].compareTo(b[0]);
 		}
-		});
-		int survivalTotal = 0;
-		int diedTotal = 0;
-		for (int row = 0; row <k; row ++) {
-			double survivalNum = arr[row][1];
-			if (survivalNum == survived ) {
-				survivalTotal++;
-			} else {
-				diedTotal = 0;
-			}
-		} 
-		
-		if(survivalTotal>diedTotal) {
-			return String.valueOf(survived);
+	});
+	
+	double survivalTotal = 0.0; 
+	double diedTotal = 0.0;
+	for (int row = 0; row <k; row ++) {
+		double survivalNum = arr[row][1];
+		if (survivalNum == 1) {
+			survivalTotal++;
 		} else {
-			return String.valueOf(died);
+			diedTotal++;
 		}
 	}
 	
-		private double getDistance(DataPoint point1, DataPoint point2) {
-			double distance = 0.0;
-			double num = 0.0;
-			double x1 = point1.getF1();
-			double y1 = point1.getF2();
-			double x2 = point2.getF1();
-			double y2 = point2.getF2();
-			num = Math.pow((x2-x1), 2) + Math.pow((y2-y1),2);
-			distance = Math.sqrt(num);
-			return distance;
-		}
-		private static class DataAnalysis {
-			int truePositive = 0;
-			int falsePositive = 0;
-			int falseNegative = 0;
-			int trueNegative = 0;
-		}
+	if (survivalTotal>diedTotal) {
+		return "1";
+	} else {
+		return "0";
+	}
+}
+	
+	private double getDistance(DataPoint x, DataPoint y) {
+		double distance = Math.sqrt(Math.pow(y.getF2() - x.getF2(), 2) + Math.pow(y.getF1() - x.getF1(), 2));
+		return distance;
+	}
 		
-		private DataAnalysis analyze(ArrayList<DataPoint> data) {
-			DataAnalysis results = new DataAnalysis();
-			for (DataPoint point : data) {
-				if (!point.isTest()) {
-					continue;
-				}
-				
-				double actualLabel = point.getLabel();
-				
-				dataSet=data;
-				double prediction = Double.parseDouble(test(point));
-				
-				if (actualLabel == 1.0) {
-					if (prediction==1.0) {
-						results.truePositive++;
-					} else {
-						results.falsePositive++;
-					}
-				} else {
-					if (prediction ==1.0) {
-						results.falseNegative++;
-					} else {
-						results.trueNegative++;
-					}
+	//implementing getPrecision and getAccuracy
+	@Override
+	double getAccuracy(ArrayList<DataPoint> data) {
+		for (DataPoint dataPoint : data ) {
+			pointLabel = dataPoint.getLabel();
+			dataSet = data;
+			testLabel = Double.parseDouble(test(dataPoint));
+			
+			if(testLabel == 1.0) {
+				if(pointLabel == 1.0) {
+					truePositive++;
+				} else if (pointLabel == 0.0) {
+					falsePositive++;
 				}
 			}
-			return results;
-	}
-		@Override
-		public double getPrecision(ArrayList<DataPoint> data) {
-			DataAnalysis dataAnalysis=analyze(data);
-			double pr = dataAnalysis.truePositive + dataAnalysis.falseNegative;
-			if (pr ==0) {
-				throw new ArithmeticException("Error");
+			
+			if(testLabel == 0.0) {
+				if(pointLabel == 0.0) {
+					trueNegative++;
+				} else if (pointLabel == 1.0) {
+					falseNegative++;
+				}
 			}
-			double precision = (double) (dataAnalysis.truePositive)/pr;
-			return precision;
 		}
-		
-		@Override
-		public double getAccuracy(ArrayList<DataPoint> data) {
-			DataAnalysis dataAnalysis=analyze(data);
-			double completeResults = dataAnalysis.truePositive + dataAnalysis.trueNegative+ dataAnalysis.falsePositive + dataAnalysis.falseNegative;
-			if (completeResults == 0) {
-				throw new ArithmeticException("Error: Division by zero");
-			}
-			double accuracy = (double) (dataAnalysis.truePositive + dataAnalysis.trueNegative)/(completeResults);
-			return accuracy;
-		}
+		total = truePositive + falsePositive + trueNegative + falseNegative;
+		accuracy = (truePositive + trueNegative) / total;
+		return accuracy;
 	}
+	
+	@Override
+	double getPrecision(ArrayList<DataPoint> data) {
+		for (DataPoint dataPoint : data ) {
+			pointLabel = dataPoint.getLabel();
+			dataSet = data;
+			testLabel = Double.parseDouble(test(dataPoint));
+			
+			if(testLabel == 1.0) {
+				if(pointLabel == 1.0) {
+					truePositive++;
+				} else if (pointLabel == 0.0) {
+					falsePositive++;
+				}
+			}
+			
+			if(testLabel == 0.0) {
+				if(pointLabel == 0.0) {
+					trueNegative++;
+				} else if (pointLabel == 1.0) {
+					falseNegative++;
+				}
+			}
+		}
+		total = truePositive+falseNegative;
+		precision = truePositive/total;
+		return precision;
+	}
+}
